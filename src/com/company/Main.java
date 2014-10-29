80554cc4b74bd0eb02e1a1fecfdfcc7edca752d8
brian-bernal <brian.bernal13@stjohns.edu>
2014-10-28 23:55:08 -0400

    Works now. There were some very interesting other solutions I might take a look at.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,25 +3,27 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        int digit1 = 100, digit2 = 101;
         int largestPalindrome = 1;
 
-        for (int i = 100; i < 1000; i++) {
-            for (int j = 100; j < 1000; j++) {
-                if (isPalindrome(i * j))
+        for (int i = 999; i > 99; i--) {
+            for (int j = 999; j > 99; j--) {
+                if (isPalindrome(i * j) && (i * j > largestPalindrome))
                     largestPalindrome = i * j;
             }
         }
-
         System.out.println(largestPalindrome);
     }
 
     static boolean isPalindrome(int num) {
         String palindrome = num + "";
-        int middle = (palindrome.length() / 2);  //If even, last element in substring.
-        String left = palindrome.substring(0, (middle));
-        String right = palindrome.substring(middle + 1, palindrome.length());
-        right = new StringBuilder(right).reverse().toString();
+        int middle = (palindrome.length() / 2);  //
+        String right = "", left = palindrome.substring(0, (middle));  //Substring is (inclusive, exclusive)
+
+        if ((palindrome.length() & 1) != 0)  //If the number of digits is odd
+            middle += 1;  //Set the beginning element one over (to skip the lone number in the middle). If the number of digits is even, we don't want to skip any digits.
+        right = palindrome.substring(middle, palindrome.length());
+        right = new StringBuilder(right).reverse().toString();  //reverse the string to compare it
+
         return (left.compareTo(right) == 0);
     }
 }
